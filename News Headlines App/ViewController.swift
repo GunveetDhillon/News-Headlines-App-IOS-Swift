9267a329551458558ea23e94b3b149c8a7df6794
GunveetDhillon <dhillon.gunu@gmail.com>
2023-08-13 06:18:06 -0400

    Initial Commit

---
+++ News Headlines App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Headlines App
+//
+//  Created by Gunveet Singh Dhillon on 2023-08-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
